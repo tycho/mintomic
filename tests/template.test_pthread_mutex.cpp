ff7428848d10fb9a5da984861e3982a1b6aa297e
Steven Noonan <steven@uplinklabs.net>
2016-03-16 18:41:05 +0000

    tests: add pthread-based lock test for comparison
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

---
+++ tests/template.test_pthread_mutex.cpp
@@ -0,0 +1,54 @@
+// This test is based on the blog post: http://preshing.com/20121019/this-is-why-they-call-it-a-weakly-ordered-cpu
+
+#include <mintomic/mintomic.h>
+#include <mintpack/timewaster.h>
+#include <mintpack/threadsynchronizer.h>
+#include <assert.h>
+#include <stdio.h>
+
+
+#cmakedefine01 TEST_FORCE_FAIL
+
+static TimeWaster g_timeWasters[4];
+static mint_atomic${TEST_INT_BITSIZE}_t g_flag;
+static uint${TEST_INT_BITSIZE}_t g_sharedValue;
+static pthread_mutex_t g_lock;
+
+static void threadFunc(int threadNum)
+{
+    TimeWaster &tw = g_timeWasters[threadNum];
+
+    int count = 0;
+    while (count < 5000000)
+    {
+        tw.wasteRandomCycles();
+        if (!pthread_mutex_trylock(&g_lock))
+        {
+            // Lock was successful
+#if !TEST_FORCE_FAIL
+            mint_thread_fence_acquire();
+#endif
+            g_sharedValue++;
+#if !TEST_FORCE_FAIL
+            mint_thread_fence_release();
+#endif
+            pthread_mutex_unlock(&g_lock);
+            count++;
+        }
+    }
+}
+
+bool ${TEST_FUNC}(int numThreads)
+{
+    assert(numThreads <= 4);
+
+    g_flag._nonatomic = 0;
+    g_sharedValue = 0;
+    pthread_mutex_init(&g_lock, NULL);
+    ThreadSynchronizer threads(numThreads);
+    threads.run(threadFunc);
+    bool success = (g_sharedValue == (uint${TEST_INT_BITSIZE}_t) 5000000 * numThreads);
+    //if (!success)
+    //    printf(" g_sharedValue=%llu", (uint64_t) g_sharedValue);
+    return success;
+}
